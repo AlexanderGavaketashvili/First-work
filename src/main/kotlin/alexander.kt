b41399f5b40477a98513d0b41c0556c7da4ccf17
AlexanderGavaketashvili <115170804+AlexanderGavaketashvili@users.noreply.github.com>
2022-10-16 23:38:27 +0400

    Davaleba

--- src/main/kotlin/alexander.kt
+++ src/main/kotlin/alexander.kt
@@ -1,3 +1,6 @@
+import java.util.DoubleSummaryStatistics
+import kotlin.jvm.internal.Reflection
+
 fun main() {
     val f1=Point(x = 1, y = 3)
     val f2=Point(x = 2, y = 6)
@@ -8,8 +11,8 @@ fun main() {
     println(f1==f2)
 }
 class Point(x:Int, y:Int) {
-    private var x: Int = x
-    private var y: Int = y
+    var x: Int = x
+    var y: Int = y
 
     override fun toString(): String {
         return "$x / $y"
@@ -17,9 +20,17 @@ class Point(x:Int, y:Int) {
 
     override fun equals(other: Any?): Boolean =
         other is Point && other.x == x
+    fun Reflection(): String {
+        var Reflectionx= -x
+        var Reflectiony= -y
+
+        return "{$Reflectionx / $Reflectiony"
+    }
+    fun truncate(x: Double, y:Double) {
+
+    }
+
+
 }
-class Fraction() {
-    var x=null
-    var y=null
-}
+
 
